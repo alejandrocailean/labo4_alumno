617129f2c502df89d09e50fd39b78243faa20e0a
Cailean Alejandro <50883843+alejandrocailean@users.noreply.github.com>
2020-09-10 05:16:03 -0300

    n

--- src/app/Componentes/alumno/alumno.component.ts
+++ src/app/Componentes/alumno/alumno.component.ts
@@ -14,18 +14,29 @@ export class AlumnoComponent implements OnInit {
   //alumno=new Alumno('','',0,'');
 
   listaAlumnos:Alumno[]=[];
+  muestroCargarB:boolean=false;
+  muestroListarB:boolean=false;
   
   constructor() {
     
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {console.log(this.muestroCargarB);
   }  
 
  
 public locura(e){
   console.log(e);
   this.listaAlumnos.push(e);
+}
+public muestroCargar(){
+  this.muestroCargarB=true;
+  this.muestroListarB=false;
+  console.log(this.muestroCargarB);
+}
+public muestroListar(){
+  this.muestroCargarB=false;
+  this.muestroListarB=true;
 }
   
 
